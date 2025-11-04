3a4cb46bb62b4b7eb2bddcaed466b28bf6bdcaa1
Gayatri <kothawadegayatri30@gmail.com>
2025-11-04 11:51:11 +0000

    added terraform output

---
+++ output.tf
@@ -0,0 +1,14 @@
+# public ip of my ec2 instance
+output "my-ec2-publicIP" {
+  value = aws_instance.example.public_ip
+}
+
+#ec2 machine instance id 
+output "my-ec2ID" {
+  value = aws_instance.example.id
+
+}
+resource "local_file" "my_output_file" {
+  content  = aws_instance.example.public_ip
+  filename = "${path.module}/myIP.txt"
+}

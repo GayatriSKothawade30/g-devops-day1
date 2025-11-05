6e37593a206b2af853f225a4d652eaa5223bcdcd
Gayatri <kothawadegayatri30@gmail.com>
2025-11-05 06:16:52 +0000

    udpate s3 backedn

--- providers.tf
+++ providers.tf
@@ -1,10 +1,19 @@
 terraform {
+
+  backend "s3" {
+    bucket = "gsk-roche-terraform-bucket"
+    key = "dev/gsk/terraform.tfstate"
+    region = "ap-southeast-2"
+    dynamodb_table = "gsk-roche-table1"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.19.0"
     }
   }
+ 
 }
 
 provider "aws" {

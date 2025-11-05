a200af81288210af94e813dcf705c6f045454ab1
Gayatri <kothawadegayatri30@gmail.com>
2025-11-05 10:31:37 +0000

    added provisioner

--- ec2.tf
+++ ec2.tf
@@ -9,5 +9,25 @@ resource "aws_instance" "example" {
     "Name" : var.vm-name
   }
 
+#provisioner
+
+  provisioner "remote-exec"{
+    inline = [
+      "sudo dnf install git httpd -y",
+      "mkdir -p hello/terraform"
+    ]
+
 }
+
+connection {
+ type = "ssh"
+ user = "ec2-user"
+ host = self.public_ip
+ #content of private key data
+ private_key = tls_private_key.rsa-4096-example.private_key_pem
+}
+
+}
+
+
 

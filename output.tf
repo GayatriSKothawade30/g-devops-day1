54279455093b70236eb9a53d4109f0381d7998dc
Gayatri <kothawadegayatri30@gmail.com>
2025-11-05 05:01:17 +0000

    fixed ec2 tags

--- output.tf
+++ output.tf
@@ -11,4 +11,5 @@ output "my-ec2ID" {
 resource "local_file" "my_output_file" {
   content  = aws_instance.example.public_ip
   filename = "${path.module}/myIP.txt"
+  depends_on = [ aws_instance.example ]
 }
